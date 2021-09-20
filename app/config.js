03820015dc46e9cdc6724ee79fda3ccc5ec062b2
tommycravens <joe.t.cravens92@gmail.com>
2021-09-20 15:50:22 -0400

    added backend api url

--- app/config.js
+++ app/config.js
@@ -1,8 +1,8 @@
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
-  development: 'http://localhost:4741'
-}
+  production: "https://hidden-sea-77582.herokuapp.com",
+  development: "http://localhost:4741",
+};
 
 if (window.location.hostname === 'localhost') {
   apiUrl = apiUrls.development
